0252432b6ea64641f17df866224ee5d9f410c173
gurkantumer <gurkan@studionord.org>
2014-06-13 13:22:02 +0300

    v.1.0 submitted

---
+++ NordSubmit/NordSubmit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NordSubmit
+//
+//  Created by Gurkan Tumer on 13/06/14.
+//  Copyright (c) 2014 Gurkan Tumer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NordSubmit/NordSubmit/controllers/SubmitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubmitViewController.h
+//  NordSubmit
+//
+//  Created by Gurkan Tumer on 13/06/14.
+//  Copyright (c) 2014 Gurkan Tumer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubmitViewController : UIViewController
+
+@end
